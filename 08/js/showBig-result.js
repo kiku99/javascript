ef02d76dab191b4878b2e591f94d39575d098ea0
kiku99 <elsd0326@gmail.com>
2020-12-01 17:18:39 +0900

    Dom_getAttribute

--- 08/js/showBig-result.js
+++ 08/js/showBig-result.js
@@ -7,6 +7,6 @@
 
 		function chagePic() {
 				var newPic = this.src;   // click 이벤트가 발생한 대상의 src 속성 값 가져옴
-				bigPic.setAttribute("src", newPic);  // newPic 값을 큰 이미지의 src 속성에 할당.
+				cup.src = newPic;  // newPic 값을 큰 이미지의 src 속성에 할당.
 				// 윗 줄을 cup.src = newPic; 로 사용해도 됨.
 			}	
