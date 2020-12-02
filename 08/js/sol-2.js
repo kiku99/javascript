4de4239eb4ca0d32bd46ec0b3b036d170ce423d4
kiku99 <elsd0326@gmail.com>
2020-12-02 12:58:16 +0900

    DomNode_Change

--- 08/js/sol-2.js
+++ 08/js/sol-2.js
@@ -1,6 +1,15 @@
 		var buttons = document.querySelectorAll(".check");
+		var ref = true;
+		
 		for(var i=0; i<buttons.length; i++) {
 			buttons[i].addEventListener("click", function() {
-				this.parentNode.style.color = "#ccc";
+				if(ref == true)	{
+					this.parentNode.style.color = "#ccc";
+						ref = false;
+				}
+				else {
+					this.parentNode.style.color = "#111";
+						ref = true;
+				}					
 			});
-		}
+			}
